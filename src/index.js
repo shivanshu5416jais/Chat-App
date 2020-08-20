60193fa480057c383f2994db478c0b308762ce94
shivanshu5416jais <42492030+shivanshu5416jais@users.noreply.github.com>
2020-08-20 21:09:09 +0530

    Added join page and differnt room functionality implemented

--- src/index.js
+++ src/index.js
@@ -19,15 +19,22 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection');
-    socket.emit('message',generateMessage('Welcome'))
-    socket.broadcast.emit('message',generateMessage('A new user has joined'))
     
+    socket.on('join',({username,room})=>{
+        socket.join(room)
+        // socket.emit,io.emit,socket.broadcast.emit
+        // io.to.emit , socket.broadcast.to.emit
+        socket.emit('message',generateMessage('Welcome'))
+        socket.broadcast.to(room).emit('message',generateMessage(`${username} has joined`))
+    
+    })
+
     socket.on('sendMessage',(message,callback)=>{
         const filter=new Filter()
         if(filter.isProfane(message)){
             return callback('Profanity is not allowed')
         }
-        io.emit('message',generateMessage(message));
+        io.to(`abc`).emit('message',generateMessage(message));
         callback("Delivered")
     })
 
@@ -38,6 +45,8 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',()=>[
         io.emit('message',generateMessage('A user has left'))
     ])
+
+
 
 
 })
