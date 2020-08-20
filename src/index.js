8486240f125ff88eb344c248ae9be7518ae8d6a9
shivanshu5416jais <42492030+shivanshu5416jais@users.noreply.github.com>
2020-08-20 19:45:19 +0530

    rendering message along with time with the help of moment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http=require('http')
 const express=require('express')
 const socketio=require('socket.io')
 const Filter=require('bad-words')
+const {generateMessage}=require('./utils/messages')
 
 const app=express()
 const server= http.createServer(app)
@@ -18,15 +19,15 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection');
-    socket.emit('message','Welcome!')
-    socket.broadcast.emit('message','A new user has joined')
+    socket.emit('message',generateMessage('Welcome'))
+    socket.broadcast.emit('message',generateMessage('A new user has joined'))
     
     socket.on('sendMessage',(message,callback)=>{
         const filter=new Filter()
         if(filter.isProfane(message)){
             return callback('Profanity is not allowed')
         }
-        io.emit('message',message);
+        io.emit('message',generateMessage(message));
         callback("Delivered")
     })
 
@@ -35,7 +36,7 @@ io.on('connection',(socket)=>{
         callback()
     })
     socket.on('disconnect',()=>[
-        io.emit('message','A user has left')
+        io.emit('message',generateMessage('A user has left'))
     ])
 
 
