a833ed26f2746a8b5b7140fa25e94668c0a3643a
shivanshu5416jais <42492030+shivanshu5416jais@users.noreply.github.com>
2020-08-21 11:50:21 +0530

    done

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const server= http.createServer(app)
 const io=socketio(server)
 
 
-const port=process.env.port || 3000
+const port=process.env.PORT || 3000
 const publicDirectoryPath=path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
